24425a5f43ae72da4f09c937a4738b7f98fdc531
Joshua Martin <martin.joshua2410@gmail.com>
2025-05-15 12:24:30 +0100

    completed basic homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { Alert, Pressable, Text, View, StyleSheet, Button } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      marginHorizontal: 16,
+    },
+    text: {
+      justifyContent: "center",
+    },
+  });
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <View>
+          <Text style={styles.text}>Guess The Colour</Text>
+          <Button
+            title="Yo Mama"
+            onPress={() => Alert.alert("successful button press")}
+          />
+        </View>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
